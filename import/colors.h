16efa3d029b659839f2367eede1d1317b295d54b
nicolas bertoncello <nicolas165@Outlook.it>
2024-01-20 20:53:35 +0100

    + colors.h: enum color, - colors.h: #define color

--- import/colors.h
+++ import/colors.h
@@ -1,24 +1,24 @@
 #ifndef colors_h
 #define colors_h
 
-//normal colors.
-#define BLACK 40
-#define RED 41
-#define GREEN 42
-#define YELLOW 43
-#define BLUE 44
-#define MAGENTA 45
-#define CYAN 46
-#define WHITE 47
-
-//bright colors.
-#define GRAY 100
-#define BRED 101
-#define BGREEN 102
-#define BYELLOW 103
-#define BBLUE 104
-#define BMAGENTA 105
-#define BCYAN 106
-#define BWHITE 107
+//list of all possible color codes.
+enum colors {
+    BLACK = 40,
+    RED = 41,
+    GREEN = 42,
+    YELLOW = 43,
+    BLUE = 44,
+    MAGENTA = 45,
+    CYAN = 46,
+    WHITE = 47,
+    GRAY = 100,
+    BRED = 101,
+    BGREEN = 102,
+    BYELLOW = 103,
+    BBLUE = 104,
+    BMAGENTA = 105,
+    BCYAN = 106,
+    BWHITE = 107
+};
 
 #endif
